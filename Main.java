37e9043568e2728ebe3220b6a9a29a61014db68a
167716 <101186863+167716@users.noreply.github.com>
2022-04-05 10:20:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    public static void main(String[] args) {
+    for(int i=1;i<25;i++){
+      System.out.println();
+     for(int n=0;n<i;n++) System.out.print("#");
+  }
+}
