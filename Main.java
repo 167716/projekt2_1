8dc01a2420404b11ca6e1ed9336eddf50f040129
167716 <101186863+167716@users.noreply.github.com>
2022-04-05 11:19:14 +0000

    3

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
 class Main {
   public static void main(String[] args) {
     int s=1;
+    int a=3;
     for(int i=5;i!=1;i--){
-      for(int k=3;k!=0;k--) System.out.print(" ");
+      for(int k=3;k!=a;k--) System.out.print(" ");
        
        for(int l=0;l!=s;l++) System.out.print("*");
       
       System.out.println();
       s++;
+      a--;
       }
        
     
