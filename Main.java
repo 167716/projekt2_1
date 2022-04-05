0acd1d22529d70acf05170fba8962738948798cb
167716 <101186863+167716@users.noreply.github.com>
2022-04-05 10:51:04 +0000

    1

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
 class Main {
   public static void main(String[] args) {
-    public static void main(String[] args) {
-    for(int i=1;i<25;i++){
+    int s=1;
+   /* for(int i=5;i!=1;i--){
+      for(int k=i-2;k!=0;k--)System.out.print(" ");
+     for(int n=1;n<5;n++){
+       
+       if(n>=s){
+         System.out.print("*");
+         s++;
+       }
+       else System.out.println();
+     }*/
+    for(int i=5;i!=1;i--) {
+      for(int k=i-2;i!=1;i--)System.out.print(" ");
+      System.out.println("*");
+    }
+       
+    
+    for(int i=1;i<5;i++){ 
+      System.out.print("   ");
+      for(int n=0;n<i;n++) System.out.print("*");
       System.out.println();
-     for(int n=0;n<i;n++) System.out.print("#");
+      }
   }
 }
